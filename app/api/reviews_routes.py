d85932de0de4080eff693818256c85e5ea2cc2c0
EricSapsford <130702784+EricSapsford@users.noreply.github.com>
2023-08-28 13:13:29 -0400

    Update reviews_routes.py
    added delete review route

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -5,67 +5,15 @@ from ..forms.review_form import ReviewForm
 from ..forms.review_form import ReviewForm
 import datetime
 
-restaurant_routes = Blueprint("restaurants", __name__)
+review_routes = Blueprint("reviews", __name__)
 
+@review_routes.route("/<int:id>/delete", methods=["DELETE"])
+@login_required
+def delete_review(id):
+    review = Review.query.filter(Review.user_id == current_user.id).get(id)
 
-
-# @restaurant_routes.route("/<int:id>/reviews", methods=["GET"])
-# def get_reviews(id):
-#     reviews = Review.query.filter(Review.restaurant_id == id).all()
-#     res = {"reviews": [rev.to_dict() for rev in reviews]}
-#     return res
-
-
-# @restaurant_routes.route("/<int:id>/reviews", methods=["POST"])
-# @login_required
-# def create_review(id):
-#     form = ReviewForm()
-#     form['csrf_token'].data = request.cookies['csrf_token']
-#     past_orders = Order.query.filter(Order.user_id == current_user.id).all()
-#     if (not len(past_orders)):
-#         return {'message': 'You must have ordered from a restaurant to create a review'}, 403
-#     if form.validate_on_submit():
-#         new_review = Review(
-#             user_id=current_user.id,
-#             restaurant_id=id,
-#             review_text=form.data["review_text"],
-#             stars=form.data['stars']
-#         )
-#         db.session.add(new_review)
-#         db.session.commit()
-#         return new_review.to_dict()
-#     if form.errors:
-#         return {"errors": form.errors}, 404
-
-
-# @restaurant_routes.route("/<int:id>/reviews", methods=["GET"])
-# def get_reviews(id):
-#     reviews = Review.query.filter(Review.restaurant_id == id).all()
-#     res = {"reviews": [rev.to_dict() for rev in reviews]}
-#     return res
-
-# @restaurant_routes.route("/<int:id>/reviews", methods=["POST"])
-# @login_required
-# def create_review(id):
-#     form = ReviewForm()
-#     form['csrf_token'].data = request.cookies['csrf_token']
-#     past_orders = Order.query.filter(Order.user_id == current_user.id).all()
-#     past_reviews = Review.query.filter(Review.user_id == current_user.id).all()
-#     if (len(past_reviews)):
-#         return {'message':'You can only submit one review for a restaurant'}, 403
-#     if (not len(past_orders)):
-#         return {'message': 'You must have ordered from a restaurant to create a review'}, 403
-#     if form.validate_on_submit():
-#         new_review = Review(
-#             user_id=current_user.id,
-#             restaurant_id=id,
-#             review_text=form.data["review_text"],
-#             stars=form.data['stars'],
-#             created_at=datetime.datetime.now(),
-#             updated_at=datetime.datetime.now()
-#         )
-#         db.session.add(new_review)
-#         db.session.commit()
-#         return new_review.to_dict()
-#     if form.errors:
-#         return {"errors": form.errors}, 404
+    if review:
+      db.session.delete(review)
+      db.session.commit()
+    else:
+      return {"message": "Review couldn't be found"}, 404
