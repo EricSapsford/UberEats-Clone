01dcdab4bb7689904bd97f8930d3f7cd4b035e6d
Josh Pascual <joshapascual@gmail.com>
2023-08-31 16:14:46 -0700

    footer name links added

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -13,18 +13,72 @@ export default function Footer() {
             <div id="footer">
                 {/* <h1>footer</h1> */}
                 <div id='footer-col-1'>
-                    <span id='footer-logo-text'>
+                    <div id='footer-logo-text'>
                         VancouberEats
-                    </span>
+                    </div>
                 </div>
-                <div id='footer-col-2'>
-                    <Link exact to="/restaurants">
-                        <div>Restaurants near me</div>
-                    </Link>
-                    {!sessionUser && (<div id='footerModal'><OpenModalButton
-                        buttonText='Create an Account'
-                        modalComponent={<SignupFormModal />} />
-                    </div>)}
+                <div className='footer-right'>
+                    <div id='footer-col-3'>
+                        <div className='footer-right-header'>Features</div>
+                        <div>
+                            <Link exact to="/restaurants">
+                                <div>Restaurants near me</div>
+                            </Link>
+                            {!sessionUser && (<div id='footerModal'><OpenModalButton
+                                buttonText='Create an Account'
+                                modalComponent={<SignupFormModal />} />
+                            </div>)}
+                        </div>
+                    </div>
+                    <div id='footer-col-2'>
+                        <div className='footer-right-header'>Contact Us</div>
+                        <div className='footer-name-list'>
+                            <div className='footer-name'>
+                                <div>Will Campbell</div>
+                                <div className='footer-links'>
+                                    <a href="" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-linkedin"></i>
+                                    </a>
+                                    <a href="" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className='footer-name'>
+                                <div>Erica Maranowski</div>
+                                <div className='footer-links'>
+                                    <a href="" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-linkedin"></i>
+                                    </a>
+                                    <a href="" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className='footer-name'>
+                                <div>Josh Pascual</div>
+                                <div className='footer-links'>
+                                    <a href="https://www.linkedin.com/in/josh-pascual/" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-linkedin"></i>
+                                    </a>
+                                    <a href="https://github.com/joshpas24" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className='footer-name'>
+                                <div>Eric Sapsford</div>
+                                <div className='footer-links'>
+                                    <a href="" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-linkedin"></i>
+                                    </a>
+                                    <a href="" target="_blank" rel="noopener noreferrer">
+                                        <i class="fa-brands fa-github"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div id='footer-copyright'>
